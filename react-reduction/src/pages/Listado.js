28e4e6c3562a9697a4367cf8bd26d59e6e667af9
Hernan Casanova <hernan.casanova.silva@outlook.com>
2021-03-30 16:17:04 -0300

    Avance: listado muestra imagen de prueba

--- react-reduction/src/pages/Listado.js
+++ react-reduction/src/pages/Listado.js
@@ -3,20 +3,17 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
 import * as actions from '../actions/VacunoActions';
+import ReactTable from 'react-table';
 import {Button} from 'reactstrap';
 import NavDropdown from 'reactstrap/lib/NavDropdown';
 
-//const dispatch = useDispatch();
-/*useEffect(()=>{
-  dispatch(listadoAnimales());
-},[]);*/
 
 const Listado = ({listadoAnimales}) => {
   const dispatch = useDispatch();
   useEffect(()=>{
     dispatch(actions.listadoAnimales());
   },[]);
-  const vacunos = useSelector(store=>store.vacuno.vacunos);
+  var vacunos = useSelector(store=>store.vacuno.vacunos);
   console.log("VACUNOS DESDE EL BODY DEL COMPONENTE FUNCIONAL LISTADO: ",vacunos);
   return (
     <Page
@@ -30,7 +27,7 @@ const Listado = ({listadoAnimales}) => {
           <Card className="mb-3">
             <CardHeader>Actualizado al </CardHeader>
             <CardBody>
-              <Table>
+              {<Table>
                 <thead>
                   <tr>
                     <th scope="col">N°</th>
@@ -38,73 +35,10 @@ const Listado = ({listadoAnimales}) => {
                     <th scope="col">Fecha nacimiento</th>
                     <th scope="col">Sexo</th>
                     <th scope="col">Raza</th>
-                    {/*<th scope="col">Estado</th>
-                    <th scope="col">Fecha Venta</th>*/}
+                    <th scope="col">Imagen</th>
                   </tr>
                 </thead>
                 <tbody>
-                  {/*
-                  <tr className="table-active">
-                    <th scope="row">Active</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Default</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                  </tr>
-
-                  <tr className="table-primary">
-                    <th scope="row">Primary</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                  </tr>
-                  <tr className="table-secondary">
-                    <th scope="row">Secondary</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                  </tr>
-                  <tr className="table-success">
-                    <th scope="row">Success</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                  </tr>
-                  <tr className="table-danger">
-                    <th scope="row">Danger</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                  </tr>
-                  <tr className="table-warning">
-                    <th scope="row">Warning</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                  </tr>
-                  <tr className="table-info">
-                    <th scope="row">Info</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                  </tr>
-                  <tr className="table-light">
-                    <th scope="row">Light</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                  </tr>
-                  <tr className="table-dark">
-                    <th scope="row">Dark</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                  </tr>*/}
                   {vacunos.map(vac=>{
                     return (<tr key={vac.id} className="table-info">
                     <th>{vac.id}</th>
@@ -112,9 +46,10 @@ const Listado = ({listadoAnimales}) => {
                     <td>{vac.fecha_nacimiento}</td>
                     <td>{vac.sexo}</td>
                     <td>{vac.raza}</td>
+                    <td><img height={34} src="https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg"/></td>  
                   </tr>);} )}
                 </tbody>
-              </Table>
+              </Table>}
             </CardBody>
           </Card>
         </Col>
