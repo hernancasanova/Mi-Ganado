e0ea303ea4ebcb4f5aaa79123fadae4e83784836
HERNAN-CASANOVA\Hernan-Casanova <hernan.casanova.silva@outlook.com>
2021-09-16 08:20:56 -0300

    En funcionamiento edicion de vacunos

--- react-reduction/src/App.js
+++ react-reduction/src/App.js
@@ -1,82 +1,70 @@
 import { STATE_LOGIN, STATE_SIGNUP } from 'components/AuthForm';
-import GAListener from 'components/GAListener';
 import { EmptyLayout, LayoutRoute, MainLayout } from 'components/Layout';
 import PageSpinner from 'components/PageSpinner';
 import AuthPage from 'pages/AuthPage';
 import React from 'react';
 import componentQueries from 'react-component-queries';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './styles/reduction.scss';
-import AuthForm from './components/AuthForm';
-import { createBrowserHistory } from 'history';
 import { ConnectedRouter } from 'connected-react-router';
 import { connect } from 'react-redux';
-import { push } from 'connected-react-router'
 
-
-const AlertPage = React.lazy(() => import('pages/AlertPage'));
-const AuthModalPage = React.lazy(() => import('pages/AuthModalPage'));
-const BadgePage = React.lazy(() => import('pages/BadgePage'));
-const ButtonGroupPage = React.lazy(() => import('pages/ButtonGroupPage'));
-const ButtonPage = React.lazy(() => import('pages/ButtonPage'));
-const CardPage = React.lazy(() => import('pages/CardPage'));
-const ChartPage = React.lazy(() => import('pages/ChartPage'));
-//const DashboardPage = React.lazy(() => import('pages/DashboardPage'));
-const DropdownPage = React.lazy(() => import('pages/DropdownPage'));
-const FormPage = React.lazy(() => import('pages/FormPage'));
-const InputGroupPage = React.lazy(() => import('pages/InputGroupPage'));
-const ModalPage = React.lazy(() => import('pages/ModalPage'));
 //const Bienvenida = React.lazy(() => import('pages/Bienvenida'));
 const Listado = React.lazy(() => import('pages/Listado'));
 const RegistrarVacuno = React.lazy(() => import('pages/RegistrarVacuno'));
 //const RegistrarVacuno = React.lazy(() => import('pages/ReactHookForm'));
 const RegistrarArete = React.lazy(() => import('pages/RegistrarArete'));
-const TypographyPage = React.lazy(() => import('pages/TypographyPage'));
-const WidgetPage = React.lazy(() => import('pages/WidgetPage'));//
-
-const getBasename = () => {
-  return `/${process.env.PUBLIC_URL.split('/').pop()}`;
-};
 
 class App extends React.Component {
-  componentDidMount(){
-    let api_token=localStorage.getItem('api_token');
-    if(!api_token){
-      this.props.history.push("login");
+  componentDidMount() {
+    let api_token = localStorage.getItem('api_token');
+    if (!api_token) {
+      this.props.history.push('login');
     }
   }
   render() {
-    const {history} = this.props;
+    const { history, vacunoEditado } = this.props;
     return (
-      <ConnectedRouter history={history} >
-          <Switch>
-            <LayoutRoute
-              exact
-              path="/login"
-              layout={EmptyLayout}
-              component={props => (
-                <AuthPage {...props} authState={STATE_LOGIN} history={history} />
-              )}
-            />
-            <LayoutRoute
-              exact
-              path="/signup"
-              layout={EmptyLayout}
-              component={props => (
-                <AuthPage {...props} authState={STATE_SIGNUP} history={history}/>
-              )}
-            />
+      <ConnectedRouter history={history}>
+        <Switch>
+          <LayoutRoute
+            exact
+            path="/login"
+            layout={EmptyLayout}
+            component={props => (
+              <AuthPage {...props} authState={STATE_LOGIN} history={history} />
+            )}
+          />
+          <LayoutRoute
+            exact
+            path="/signup"
+            layout={EmptyLayout}
+            component={props => (
+              <AuthPage {...props} authState={STATE_SIGNUP} history={history} />
+            )}
+          />
 
-            <MainLayout breakpoint={this.props.breakpoint} history={history}>
-              <React.Suspense fallback={<PageSpinner />}>
-                {/*<Route exact path="/" component={Bienvenida} />*/}
-                <Route exact path="/listado_vacunos" component={Listado} />
-                <Route exact path="/registrar_vacuno" component={RegistrarVacuno} />
-                <Route exact path="/registrar_arete" component={RegistrarArete} />
-                {/*<Redirect </Redirect>to={} /> */}
-              </React.Suspense>
-            </MainLayout>
-          </Switch>
+          <MainLayout breakpoint={this.props.breakpoint} history={history}>
+            <React.Suspense fallback={<PageSpinner />}>
+              {/*<Route exact path="/" component={Bienvenida} />*/}
+              <Route exact path="/listado_vacunos" component={Listado} />
+              <Route
+                exact
+                path="/registrar_vacuno"
+                component={RegistrarVacuno}
+              />
+              {vacunoEditado.id > 0 && (
+                <Route
+                  exact
+                  path="/editar_vacuno"
+                  component={RegistrarVacuno}
+                />
+              )}
+              <Route exact path="/registrar_arete" component={RegistrarArete} />
+              {/*<Redirect </Redirect>to={} /> */}
+            </React.Suspense>
+          </MainLayout>
+        </Switch>
       </ConnectedRouter>
     );
   }
@@ -106,4 +94,8 @@ const query = ({ width }) => {
   return { breakpoint: 'xs' };
 };
 
-export default componentQueries(query)(App);
+const mapStateToProps = state => ({
+  vacunoEditado: state.vacuno.vacunoEditado,
+});
+
+export default connect(mapStateToProps, {})(componentQueries(query)(App));
--- react-reduction/src/pages/Listado.js
+++ react-reduction/src/pages/Listado.js
@@ -1,57 +1,70 @@
 import Page from 'components/Page';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Card, CardBody, CardHeader, Col, Row, Table, Button, Modal, ModalBody, Spinner, ModalFooter, ModalHeader } from 'reactstrap';
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  Row,
+  Table,
+  Button,
+} from 'reactstrap';
 import * as actions from '../actions/VacunoActions';
-import { jsPDF } from "jspdf";
+import { jsPDF } from 'jspdf';
 import 'jspdf-autotable';
 import PageSpinner from '../components/PageSpinner';
 import Swal from 'sweetalert2';
-import * as moment from 'moment';
+import { FaPencilAlt, FaTrash } from 'react-icons/fa';
 
-const Listado = (props) => {  
+const Listado = props => {
   const styles = {
-      width: 300,
-      height: 300,
-      resizeMode: 'cover',
-      textAlign: 'center',
-      verticalAlign: 'middle' 
+    width: 300,
+    height: 300,
+    resizeMode: 'cover',
+    textAlign: 'center',
+    verticalAlign: 'middle',
   };
   var fechaFormateada;
-  var url_imagenes = useSelector(store=>store.vacuno.url_imagenes);
-  var loading = useSelector(store=>store.vacuno.loading);
-  var vacunosBuscados = useSelector(store=>store.vacuno.vacunosBuscados);
+  var vacunos = useSelector(store => store.vacuno.vacunos);
+  var url_imagenes = useSelector(store => store.vacuno.url_imagenes);
+  var loading = useSelector(store => store.vacuno.loading);
+  var vacunosBuscados = useSelector(store => store.vacuno.vacunosBuscados);
   const descargarPdf = () => {
-    console.log("Descargando pdf");
+    console.log('Descargando pdf');
     var pdf = new jsPDF('p', 'pt', 'letter');
-    var nColumna=1;
-    var ids=[];
-    var i=0;
+    var nColumna = 1;
+    var ids = [];
+    var i = 0;
     //var fecha;
+    let tabla = document.getElementById('listVacunos'),
+      filas = tabla.rows;
+    for (var j = 0; j < filas.length; j++) {
+      filas[j].deleteCell(8);
+    }
     pdf.autoTable({
-      html: "#listVacunos",
+      html: tabla,
       columnStyles: {
-        0: {cellWidth: 'auto', minCellHeight: 80},
-        1: {cellWidth: 120, minCellHeight: 80},
-        2: {cellWidth: 'auto', minCellHeight: 80},
-        3: {cellWidth: 'auto', minCellHeight: 80},
-        4: {cellWidth: 'auto', minCellHeight: 80},
-        5: {cellWidth: 'auto', minCellHeight: 80},
-        6: {cellWidth: 'auto', minCellHeight: 80},
-        7: {cellWidth: 'auto', minCellHeight: 80},
+        0: { cellWidth: 'auto', minCellHeight: 80 },
+        1: { cellWidth: 120, minCellHeight: 80 },
+        2: { cellWidth: 'auto', minCellHeight: 80 },
+        3: { cellWidth: 'auto', minCellHeight: 80 },
+        4: { cellWidth: 'auto', minCellHeight: 80 },
+        5: { cellWidth: 'auto', minCellHeight: 80 },
+        6: { cellWidth: 'auto', minCellHeight: 80 },
+        7: { cellWidth: 'auto', minCellHeight: 80 },
         // etc
       },
-      styles:{
+      styles: {
         valign: 'middle',
-        halign : 'center'
+        halign: 'center',
       },
       //bodyStyles: {minCellHeight: 80, minCellWidth: 80},
       includeHiddenHtml: true,
-      didParseCell: function(data) {
-        console.log("data: ",data);
-        if(data.column.index===0 && data.cell.section === 'body'){
+      didParseCell: function (data) {
+        if (data.column.index === 0 && data.cell.section === 'body') {
           ids.push(data.cell.text[0]);
-          data.cell.text[0]=nColumna.toString();
+          data.cell.text[0] = nColumna.toString();
           nColumna++;
         }
         /*if((data.column.index === 4 && data.cell.section === 'body')||(data.column.index === 5 && data.cell.section === 'body')){
@@ -62,41 +75,55 @@ const Listado = (props) => {
           }
         }*/
       },
-      didDrawCell: function(data) {
-        if(data.column.index === 1 && data.cell.section === 'body') {
-          pdf.addImage("http://localhost:8000/api/vacunos/"+ids[i], 'JPEG', data.cell.x + 15, data.cell.y + 2, 80, 80);
+      didDrawCell: function (data) {
+        if (data.column.index === 1 && data.cell.section === 'body') {
+          pdf.addImage(
+            'http://localhost:8000/api/vacunos/' + ids[i],
+            'JPEG',
+            data.cell.x + 15,
+            data.cell.y + 2,
+            80,
+            80,
+          );
           i++;
         }
-      }
+      },
     });
-    pdf.save("Listado_animales.pdf");
-  }
+    pdf.save('Listado_animales.pdf');
+  };
   const dispatch = useDispatch();
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(actions.listadoAnimales());
-  },[]);
-  useEffect(()=>{
-    //dispatch(actions.listadoAnimales());
-    if(vacunosBuscados && vacunos.length===0){
+  }, []);
+  useEffect(() => {
+    if (!loading && vacunosBuscados && vacunos.length === 0) {
       Swal.fire({
-        'icon': 'info',
-        'title': 'No se encontraron vacunos',
-        'confirmButtonText': 'Registrar vacuno',
-        'footer': '<a href="javascript:location.reload(true)">Favor recargue la página si existe algun problema</a>',
-        'allowOutsideClick': false
-      }).then((result) => {
+        icon: 'info',
+        title: 'No se encontraron vacunos',
+        confirmButtonText: 'Registrar vacuno',
+        footer:
+          '<a href="javascript:location.reload(true)">Favor recargue la página si existe algun problema</a>',
+        allowOutsideClick: false,
+      }).then(result => {
         if (result.isConfirmed) {
-          props.history.push("registrar_vacuno");
+          props.history.push('registrar_vacuno');
         }
-      })
+      });
     }
-  },[vacunosBuscados]);
-  var vacunos = useSelector(store=>store.vacuno.vacunos);
-  var url_imagenes = useSelector(store=>store.vacuno.url_imagenes); 
-  const formatoFecha = (fecha) => {
-    fechaFormateada=fecha.split('-');
-    return [fechaFormateada[2],fechaFormateada[1],fechaFormateada[0] ].join("-");
-  }
+  }, [vacunosBuscados]);
+  const formatoFecha = fecha => {
+    fechaFormateada = fecha.split('-');
+    return [fechaFormateada[2], fechaFormateada[1], fechaFormateada[0]].join(
+      '-',
+    );
+  };
+  const editarVacuno = vac => {
+    dispatch(actions.vacunoSeleccionado(vac));
+    props.history.push('editar_vacuno');
+  };
+  const eliminarVacuno = vac => {
+    console.log('vacuno eliminado: ' + vac);
+  };
   return (
     <Page
       title="Listado de vacunos"
@@ -107,43 +134,77 @@ const Listado = (props) => {
         <Col>
           <Card className="mb-3">
             <CardHeader>
-              <Button disabled={vacunos.length===0} style={{float:'right'}} className="ml-10" onClick={descargarPdf}>Descargar pdf </Button>
+              <Button
+                disabled={vacunos.length === 0}
+                style={{ float: 'right' }}
+                className="ml-10"
+                onClick={descargarPdf}
+              >
+                Descargar pdf{' '}
+              </Button>
               {/*<Button disabled={vacunos.length===0} style={{float:'right'}} className="ml-10" onClick={descargarPdf}>{loading?"Descargar pdf":<>{"Generando pdf...  "}<Spinner/></>}</Button>*/}
             </CardHeader>
-            <CardBody>{loading?<PageSpinner texto="Cargando listado" />:
-              <Table id="listVacunos">
-                <thead>
-                  <tr >
-                    <th scope="col" hidden={true}>N°</th>
-                    <th scope="col">Imagen</th>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">DIIO</th>
-                    <th scope="col">Fecha colocación</th>
-                    <th scope="col">Fecha nacimiento</th>
-                    <th scope="col">Sexo</th>
-                    <th scope="col">Color</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {vacunos.map(vac=>{
-                    return (<tr key={vac.id} >
-                    <td hidden={true}>{vac.id}</td>
-                    <td><img height={styles.height} width={styles.width} src={url_imagenes+vac.id+".jpg"}/></td>
-                    <td >{vac.nombre}</td>
-                    <td>{vac.numero!=null?vac.numero:"Sin arete"}</td>
-                    <td>{vac.fecha_colocacion!=null?formatoFecha(vac.fecha_colocacion): "Sin arete"}</td>
-                    <td>{formatoFecha(vac.fecha_nacimiento)}</td>
-                    <td>{vac.sexo}</td>
-                    <td>{vac.raza}</td>
-                  </tr>);} )}
-                </tbody>
-              </Table>}
+            <CardBody>
+              {loading ? (
+                <PageSpinner texto="Cargando listado" />
+              ) : (
+                <Table id="listVacunos">
+                  <thead>
+                    <tr>
+                      <th scope="col" hidden={true}>
+                        N°
+                      </th>
+                      <th scope="col">Imagen</th>
+                      <th scope="col">Nombre</th>
+                      <th scope="col">DIIO</th>
+                      <th scope="col">Fecha colocación</th>
+                      <th scope="col">Fecha nacimiento</th>
+                      <th scope="col">Sexo</th>
+                      <th scope="col">Color</th>
+                      <th scope="col">Acciones</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {vacunos.map(vac => {
+                      return (
+                        <tr key={vac.id}>
+                          <td hidden={true}>{vac.id}</td>
+                          <td>
+                            <img
+                              alt={vac.name}
+                              height={styles.height}
+                              width={styles.width}
+                              src={url_imagenes + vac.id + '.jpg'}
+                            />
+                          </td>
+                          <td>{vac.nombre}</td>
+                          <td>
+                            {vac.numero != null ? vac.numero : 'Sin arete'}
+                          </td>
+                          <td>
+                            {vac.fecha_colocacion != null
+                              ? formatoFecha(vac.fecha_colocacion)
+                              : 'Sin arete'}
+                          </td>
+                          <td>{formatoFecha(vac.fecha_nacimiento)}</td>
+                          <td>{vac.sexo}</td>
+                          <td>{vac.color}</td>
+                          <td>
+                            <FaPencilAlt onClick={() => editarVacuno(vac)} />{' '}
+                            <FaTrash onClick={() => eliminarVacuno(vac)} />
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </Table>
+              )}
             </CardBody>
           </Card>
         </Col>
       </Row>
     </Page>
-                    );
-}
+  );
+};
 
 export default Listado;
