23a7049094d06dd16f7d274da0cf059faead1678
HERNAN-CASANOVA\Hernan-Casanova <hernan.casanova.silva@outlook.com>
2021-10-23 18:44:28 -0300

    Modificación pasos para ejecutar aplicación

--- README.md
+++ README.md
@@ -6,15 +6,13 @@
 # Pasos para ejecutar la aplicación
 
 1.  Clonar repositorio `git clone https://github.com/hernancasanova/Mi-Ganado.git`
-2.  Ingresar a la carpeta rest y ejecutar los pasos 3,4,5,6,7,8,9,10
+2.  Ingresar a la carpeta rest y ejecutar los pasos 3,4,5,6,7,8
 3.  Crear archivo '.env' renombrando el archivo '.env.example'
 4.  Construir imagen de la API: `docker-compose build app`
 5.  Levantar servicios del stack de backend: `docker-compose up -d`
 6.  Instalar dependencias de php: `docker-compose exec app composer install`
 7.  Generar clave de la aplicación de API: `docker-compose exec app php artisan key:generate`
-8.  Ejecutar migraciones: `docker-compose exec app php artisan migrate`
-9.  Ejecutar seeders: `docker-compose exec app php artisan db:seed `
-10. Crear enlace simbolico a carpeta de imagenes: `docker-compose exec app php artisan storage:link`
-11. Ingresar a la carpeta react-reduction ubicada en el mismo nivel que la carpeta rest y ejecutar los siguientes pasos
-12. Crear contenedor de la aplicación de react e instalar dependencias: `docker-compose run --rm frontend npm install`
-13. Levantar contenedor de la aplicación de react: `docker-compose up -d`
+8.  Crear enlace simbolico a carpeta de imagenes: `docker-compose exec app php artisan storage:link`
+9.  Ingresar a la carpeta react-reduction ubicada en el mismo nivel que la carpeta rest y ejecutar los siguientes pasos
+10. Crear contenedor de la aplicación de react e instalar dependencias: `docker-compose run --rm frontend npm install`
+11. Levantar contenedor de la aplicación de react: `docker-compose up -d`
---
+++ rest/docker-compose/mysql/MiGanadoDB.sql
@@ -0,0 +1,204 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: miganado
+-- ------------------------------------------------------
+-- Server version	5.7.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `aretes`
+--
+
+DROP TABLE IF EXISTS `aretes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aretes` (
+  `numero` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `vacuno_id` bigint(20) unsigned NOT NULL,
+  `fecha_colocacion` date NOT NULL,
+  `estado` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'activo',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`numero`),
+  KEY `aretes_vacuno_id_foreign` (`vacuno_id`),
+  CONSTRAINT `aretes_vacuno_id_foreign` FOREIGN KEY (`vacuno_id`) REFERENCES `vacunos` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aretes`
+--
+
+LOCK TABLES `aretes` WRITE;
+/*!40000 ALTER TABLE `aretes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aretes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+DROP TABLE IF EXISTS `failed_jobs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `failed_jobs`
+--
+
+LOCK TABLES `failed_jobs` WRITE;
+/*!40000 ALTER TABLE `failed_jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `failed_jobs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migrations`
+--
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` VALUES (1,'2014_10_12_000000_create_users_table',1),(2,'2019_08_19_000000_create_failed_jobs_table',1),(3,'2020_08_10_201555_add_apitoken_to_users_table',1),(4,'2021_03_24_222050_create_tipos_vacunos_table',1),(5,'2021_03_25_115526_create_vacunos_table',1),(6,'2021_03_25_134345_create_aretes_table',1),(7,'2021_05_04_120556_add_estado_to_aretes_table',1);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipos_vacunos`
+--
+
+DROP TABLE IF EXISTS `tipos_vacunos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tipos_vacunos` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre_tipo_vacuno` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipos_vacunos`
+--
+
+LOCK TABLES `tipos_vacunos` WRITE;
+/*!40000 ALTER TABLE `tipos_vacunos` DISABLE KEYS */;
+INSERT INTO `tipos_vacunos` VALUES (1,'Ternero',NULL,NULL),(2,'Ternera',NULL,NULL),(3,'Toro',NULL,NULL),(4,'Vaquilla',NULL,NULL),(5,'Vaca',NULL,NULL),(6,'Buey',NULL,NULL),(7,'Novillo',NULL,NULL);
+/*!40000 ALTER TABLE `tipos_vacunos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `api_token` varchar(80) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`),
+  UNIQUE KEY `users_api_token_unique` (`api_token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vacunos`
+--
+
+DROP TABLE IF EXISTS `vacunos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vacunos` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `sexo` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tipos_vacunos_id` bigint(20) unsigned NOT NULL,
+  `color` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `estado` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fecha_venta` date DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `vacunos_tipos_vacunos_id_foreign` (`tipos_vacunos_id`),
+  CONSTRAINT `vacunos_tipos_vacunos_id_foreign` FOREIGN KEY (`tipos_vacunos_id`) REFERENCES `tipos_vacunos` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vacunos`
+--
+
+LOCK TABLES `vacunos` WRITE;
+/*!40000 ALTER TABLE `vacunos` DISABLE KEYS */;
+INSERT INTO `vacunos` VALUES (1,'Toro amarillo','2019-03-05','Macho',3,'Amarillo(a)','Vivo',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `vacunos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'miganado'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-23 18:38:57
